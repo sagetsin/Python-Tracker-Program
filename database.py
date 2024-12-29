7b218670d1a5765fe685e4d1c3bfd6506626efe2
Sydney Tsin <sage.tsin@gmail.com>
2024-12-28 19:07:56 -0500

    Insert values to the table

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-#!.\venv\Scripts\python.exe
+#!/usr/bin/env python
+
 '''This is python sqlite3 database 
 @author: Sydney Tsin
 '''
@@ -11,6 +12,15 @@ c = conn.cursor()
 '''Create Table in Database'''
 c.execute("""CREATE TABLE IF NOT EXISTS Materials 
           (p_id INTEGER PRIMARY KEY, idealNumber Integer, material TEXT, givenNumber INTEGER)""" )
+
+'''Insert multiple rows of data into the table'''
+MATERIALS = [(1, 90, 'Power', 0),
+             (2, 20, 'Defense', 0),
+             (3, 0, 'Mind', 0),
+             (4, 0, 'Evade', 0),
+             (5, 25, 'Luck', 0)]
+
+c.executemany("""INSERT INTO Materials VALUES (?,?,?,?)""", MATERIALS)
 
 conn.commit()
 conn.close()
